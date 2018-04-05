ec58e437dbbbf4f68092b0a63f27b9b883a4858e
WebbLu <u.master.o.twn@gmail.com>
2018-04-05 18:06:30 +0800

    init: todolist webapp with nuxt

---
+++ components/TodoItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <li>
+        <label :style="labelStyle">
+            <input type="checkbox" v-model="item.done" @change="$emit('done')">{{ item.label }}
+        </label>
+        <button @click="$emit('remove')">Del</button>
+    </li>
+</template>
+
+<style scoped>
+li {
+  list-style: none;
+}
+label,
+input,
+button {
+  vertical-align: middle;
+}
+button {
+  cursor: pointer;
+  background: none;
+  border: none;
+  appearance: none;
+  color: red;
+}
+</style>
+
+<script>
+export default {
+  props: ["item"],
+  computed: {
+    labelStyle() {
+      return this.item.done ? { textDecoration: "line-through" } : {};
+    }
+  }
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
